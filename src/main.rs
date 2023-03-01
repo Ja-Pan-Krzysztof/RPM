0fa14fd06ce1e72a5c4b4863e38d8fc76aea82ff
Ja <Ja_Pan_Krzysztof@gmail.com>
2023-03-01 07:10:56 +0100

    added generate password while insertion new password.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 use models::connection::create_table;
 
 
--- src/parser/arg_parser.rs
+++ src/parser/arg_parser.rs
@@ -2,6 +2,29 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 use crate::{password_generator, models};
 
 
+fn get_password(lenght: &str) -> String {
+    match lenght.parse::<u64>() {
+        Ok(len) => {
+            let config = password_generator::config::Config {
+                enable_digit: true,
+                enable_lower: true,
+                enable_upper: true,
+                enable_special: true,
+                pass_lenght: len,
+            };
+
+            let a = password_generator::gen_pass::gen_pass(config.pass_lenght, &mut config.new());
+
+            return a;
+        }
+
+        Err(error) => {
+            panic!("{}", error);
+        }
+    }
+}
+
+
 fn parse_args() -> clap::Result<ArgMatches> {
     let matches = App::new("JPK")
         .version("1.0")
@@ -17,7 +40,7 @@ fn parse_args() -> clap::Result<ArgMatches> {
                             .value_name("INTEGER")
                             .help("Set lenght passworg that will be generated (Default 10 chars)")
                             .takes_value(true)
-                        .required(false)
+                            .required(false)
                 )
         )
         .subcommand(
@@ -48,7 +71,24 @@ fn parse_args() -> clap::Result<ArgMatches> {
                         .value_name("STRING")
                         .help("The password that will be wrote in safe place")
                         .takes_value(true)
-                        .required(true),
+                        .required(false),
+                )
+                .arg(
+                    Arg::new("genpass")
+                        .short('g')
+                        .long("genpass")
+                        .help("Generate password and save in db with other parameters")
+                        .takes_value(false)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new("lenght")
+                            .short('l')
+                            .long("lenght")
+                            .value_name("INTEGER")
+                            .help("Set lenght passworg that will be generated (Default 10 chars)")
+                            .takes_value(true)
+                            .required(false)
                 )
         )
         .get_matches_safe();
@@ -62,13 +102,24 @@ pub fn options() {
     match matches {
         Ok(matches) => {
             if let Some(sub_matches) = matches.subcommand_matches("savepass") {
-                let password = sub_matches.value_of("password").unwrap_or("Idk password");
-                let site = sub_matches.value_of("site").unwrap_or("`none site`");
-                let user = sub_matches.value_of("user").unwrap_or("`none user`");
+                let site = sub_matches.value_of("site").unwrap_or("");
+                let user = sub_matches.value_of("user").unwrap_or("");
+                let mut password = String::new();
 
-                println!("password is {}", password);
-                println!("user is {}", user);
-                println!("site is {}", site);
+                if sub_matches.is_present("genpass") {
+                    let len = sub_matches.value_of("lenght").unwrap_or("10");
+
+                    password = get_password(len);
+                }
+                else {
+                    password = sub_matches.value_of("password")
+                        .unwrap_or("")
+                        .to_string();
+
+                    if password == "" {
+                        return eprintln!("[Not OK] The password must not be `None`");
+                    }
+                }
 
                 models::connection::add_new_password(
                     user.to_string(),
@@ -80,24 +131,7 @@ pub fn options() {
             else if let Some(sub_matches) = matches.subcommand_matches("genpass") {
                 let lenght = sub_matches.value_of("lenght").unwrap_or("10");
 
-                match lenght.parse::<u64>() {
-                    Ok(len) => {
-                        let config = password_generator::config::Config {
-                            enable_digit: true,
-                            enable_lower: true,
-                            enable_upper: true,
-                            enable_special: true,
-                            pass_lenght: len,
-                        };
-
-                        let a = password_generator::gen_pass::gen_pass(config.pass_lenght, &mut config.new());
-                        println!("{}", a);
-                    }
-
-                    Err(error) => {
-                        println!("{}", error);
-                    }
-                }
+                println!("[OK] {}", get_password(lenght));
             }
         }
 
